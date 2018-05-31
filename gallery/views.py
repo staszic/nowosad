8780f6d0199d206b78500ee57e94508126a1bc0d
michalstaszewski <michstaszewski@gmail.com>
2018-05-31 10:36:08 +0200

    gallery and plants refactored

--- gallery/views.py
+++ gallery/views.py
@@ -5,8 +5,5 @@ from .models import Image
 # Create your views here.
 def index(request):
     images_list = Image.objects.order_by('alt_title')
-    paginator = Paginator(images_list, 12)
-    page = request.GET.get('page')
-    images = paginator.get_page(page)
-    context = {'images_list': images}
+    context = {'images_list': images_list}
     return render(request, 'gallery/index.html', context)
