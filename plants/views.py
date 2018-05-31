a22769a5ffa20cd1729a4c38b49fc945955ea47f
michalstaszewski <michstaszewski@gmail.com>
2018-05-31 13:06:05 +0200

    plants

--- plants/views.py
+++ plants/views.py
@@ -5,10 +5,10 @@ from .models import Plant, Category
 # Create your views here.
 def index(request):
     plants = Plant.objects
-    plants_ch = plants.filter(category=3)
-    plants_by = plants.filter(category=4)
-    plants_ba = plants.filter(category=1)
-    plants_ra = plants.filter(category=2)
+    plants_ch = plants.filter(category=2)
+    plants_by = plants.filter(category=1)
+    plants_ba = plants.filter(category=3)
+    plants_ra = plants.filter(category=4)
     context = {'chryzantemy': plants_ch,
                'balkonowe': plants_ba,
                'rabatowe': plants_ra,
