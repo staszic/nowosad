98a7f53ca2e8b07129f84f52469f86197697683a
michalstaszewski <michstaszewski@gmail.com>
2018-05-31 10:58:42 +0200

    gallery and plants refactored

--- plants/views.py
+++ plants/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
 from .models import Plant, Category
 
 
 # Create your views here.
 def index(request):
     categories = Category.objects.order_by('category_name')
+    plants_by_cat = {}
     plants = Plant.objects.order_by('category', 'plant_name')
-    context = {'categories': categories, 'plants': plants}
+    for cat in categories:
+        plants_by_cat[cat] = plants.filter(category=cat)
+    context = {'categories': categories, 'plants': plants_by_cat}
     return render(request, 'plants/index.html', context)
 
 
