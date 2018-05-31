69b38982beb1a385dcb624385b19133d69f7ce2e
michalstaszewski <michstaszewski@gmail.com>
2018-05-31 12:50:40 +0200

    gallery, plants

--- plants/views.py
+++ plants/views.py
@@ -5,11 +5,18 @@ from .models import Plant, Category
 # Create your views here.
 def index(request):
     categories = Category.objects.order_by('category_name')
+    plants = Plant.objects
     plants_by_cat = {}
-    plants = Plant.objects.order_by('category', 'plant_name')
-    for cat in categories:
-        plants_by_cat[cat] = plants.filter(category=cat)
-    context = {'categories': categories, 'plants': plants_by_cat}
+    plants_ch = plants.filter(category="Chryzantemy doniczkowe")
+    plants_by = plants.filter(category="Byliny i trawy")
+    plants_ba = plants.filter(category="Kwiaty balkonowe")
+    plants_ra = plants.filter(category="Kwiaty rabatowe")
+    context = {'categories': categories,
+               'chryzantemy': plants_ch,
+               'balkonowe': plants_ba,
+               'rabatowe': plants_ra,
+               'byliny': plants_by,
+               }
     return render(request, 'plants/index.html', context)
 
 
